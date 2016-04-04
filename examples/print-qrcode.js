88e5aee8a2d4ed912bf50af68634ab128fdc0b14
taoyuan <torworx@gmail.com>
2016-04-04 12:56:26 +0800

    Update examples

--- examples/print-qrcode.js
+++ examples/print-qrcode.js
@@ -1,6 +1,5 @@
 "use strict";
 
-var path = require('path');
 var npos = require('..');
 
 // var device = new npos.Console();
@@ -8,13 +7,14 @@ var device = new npos.USB();
 var printer = new npos.Printer(device);
 
 var text = '西游记告诉我们：凡是有后台的妖怪都被接走了，凡是没后台的都被一棒子打死了。';
+
 device.open(function () {
-  npos.graphic.qrcode(text, 12).then(function (g) {
+  npos.graphic.qrcode(text, {level: 'H', size: 12}).then(function (g) {
     printer
       .align('ct')
       .bitimage(g)
       .raster(g)
-      .feed(2)
+      .feed(4)
       .flush();
   });
 });
