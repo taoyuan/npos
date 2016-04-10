e0eb4938de8ead857b8918fa4836bb1ddb2d2264
taoyuan <torworx@gmail.com>
2016-04-10 12:11:32 +0800

    Get rid of \u0000 from the result of textualize

--- test/textualize.test.js
+++ test/textualize.test.js
@@ -7,21 +7,32 @@ var s = require('./support');
 var npos = require('..');
 
 describe('textualize', function () {
-    it('should parse raster raw data', function () {
-      this.timeout(5000);
-      var parser = npos.parser();
-      var raw = fs.readFileSync(path.join(__dirname, 'fixtures', 'raster-2.bin'));
-      return parser.parse(raw).then(function (ast) {
-        assert.ok(ast);
-        return npos.textualize(ast, {
-          ocr: {
-            tessdata: s.fixtures('tessdata'),
-            language: 'pos.chs.fast'
-          }
-        }).then(function (results) {
-          assert.lengthOf(results, 1);
-        });
+  it('should parse raster raw data', function () {
+    this.timeout(5000);
+    var parser = npos.parser();
+    var raw = fs.readFileSync(path.join(__dirname, 'fixtures', 'raster-2.bin'));
+    return parser.parse(raw).then(function (ast) {
+      assert.ok(ast);
+      return npos.textualize(ast, {
+        ocr: {
+          tessdata: s.fixtures('tessdata'),
+          language: 'pos.chs.fast'
+        }
+      }).then(function (results) {
+        assert.lengthOf(results, 1);
+        assert.include(results[0], '金额');
       });
     });
+  });
+
+  it('should get rid of illegal characters', function () {
+    return npos.textualize([{
+      type: 'text',
+      data: new Buffer('h\u0000el\u0000lo\u0000 \u0000worl\u0000\u0000\u0000d')
+    }]).then(function (results) {
+      assert.lengthOf(results, 1);
+      assert.equal(results[0], 'hello world');
+    });
+  });
 
 });
