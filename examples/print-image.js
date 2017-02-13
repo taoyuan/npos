fb2e2919959579fbdd623347c0c96e3a4162c2a9
Yuan Tao <towyuan@outlook.com>
2017-02-13 23:27:34 +0800

    Fix print image source

--- examples/print-image.js
+++ examples/print-image.js
@@ -3,12 +3,12 @@
 var path = require('path');
 var npos = require('../');
 
-// var device = new npos.Console();
-var device = new npos.USB();
+var device = new npos.Console();
+// var device = new npos.USB();
 var printer = new npos.Printer(device);
 
 device.open(function () {
-  npos.graphic.fromImage(path.join(__dirname, 'fixtures', 'qrcode.bmp')).then(function (g) {
+  npos.graphic.fromImage(path.join(__dirname, 'fixtures', 'tux.png')).then(function (g) {
     printer
       .align('ct')
       .bitimage(g, 's8')
